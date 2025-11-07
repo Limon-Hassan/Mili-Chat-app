cc8caa9a2adba8ec0c7c86b135c0c2418eb0e293
Limon <mahammudhassanlimon@gmail.com>
2025-11-08 03:01:42 +0600

    work in registation

---
+++ chat-app/components/FloatingInputs.jsx
@@ -0,0 +1,63 @@
+'use client';
+import React, { useRef } from 'react';
+
+function FloatingInput({ label, type, id }) {
+  const inputRef = useRef(null);
+  const labelRef = useRef(null);
+
+  let handleFocus = () => {
+    labelRef.current.classList.add(
+      '-top-[10px]',
+      'text-[14px]',
+      'text-blue-400',
+      'bg-white',
+      'px-2',
+      'rounded-[2px]'
+    );
+    labelRef.current.classList.remove(
+      'top-1/2',
+      '-translate-y-1/2',
+      'text-white'
+    );
+  };
+
+  let handleBlur = () => {
+    if (inputRef.current.value === '') {
+      labelRef.current.classList.remove(
+        'text-[14px]',
+        'text-blue-400',
+        '-top-[10px]',
+        'bg-white',
+        'px-2',
+        'rounded-[2px]'
+      );
+      labelRef.current.classList.add(
+        'top-1/2',
+        '-translate-y-1/2',
+        'text-white'
+      );
+    }
+  };
+  return (
+    <div className="relative w-full h-[60px] mb-[30px]">
+      <input
+        type={type}
+        id={id}
+        ref={inputRef}
+        required
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        className="w-full h-full rounded-md border border-white bg-transparent px-4 text-[16px] font-semibold font-open_sens text-white outline-none focus:border-blue-500 transition-all duration-300"
+      />
+      <label
+        ref={labelRef}
+        htmlFor={id}
+        className="absolute left-4 top-1/2 -translate-y-1/2 text-white transition-all duration-300 font-semibold font-open_sens"
+      >
+        {label}
+      </label>
+    </div>
+  );
+}
+
+export default FloatingInput;
--- chat-app/components/Registation.jsx
+++ chat-app/components/Registation.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
+'use client';
+
 import Container from './container/Container';
+import FloatingInput from './FloatingInputs';
 
 const Registation = () => {
   return (
-    <section className="">
+    <section>
       <Container>
         <div className="flex justify-center items-center h-screen">
-          <div className="w-[30%] h-[30%] border border-white flex justify-center items-center">
-            <h1 className="text-[28px] font-bold font-inter text-white leading-6
-            ">
+          <div className="w-[660px] h-auto p-8 rounded-lg border border-white   shadow-2xl shadow-blue-600">
+            <h1
+              className="text-[36px] font-bold font-inter text-white leading-6
+              flex items-center justify-center mb-[50px]"
+            >
               Registation Your Account
             </h1>
-            <div className="w-[30%] h-[60px] rounded-full border border-white">
-              <input
-                className="w-full h-full text-[16px] font-semibold font-open_sens outline-none"
-                type="text"
-              />
-            </div>
+            <FloatingInput label="Username" type="text" id="name" />
+            <FloatingInput label="Email" type="email" id="email" />
+            <FloatingInput label="Password" type="password" id="password" />
+            
           </div>
         </div>
       </Container>
