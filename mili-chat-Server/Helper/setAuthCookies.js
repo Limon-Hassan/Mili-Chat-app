fa2e087585bed8cf75f32030e800157da1872eae
Limon <mahammudhassanlimon@gmail.com>
2026-01-13 14:16:30 +0600

    secure ture

--- mili-chat-Server/Helper/setAuthCookies.js
+++ mili-chat-Server/Helper/setAuthCookies.js
@@ -1,16 +1,16 @@
 function setAuthCookies(res, accessToken, refreshToken) {
   res.cookie('accessToken', accessToken, {
     httpOnly: true,
-    // secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: 'none',
+    secure: true,
     maxAge: 15 * 60 * 1000,
   });
 
   if (refreshToken) {
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
-      // secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      secure: true,
+      sameSite: 'none',
       maxAge: 24 * 60 * 60 * 1000,
     });
   }
